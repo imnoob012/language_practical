375e246f173d13e9814c643d8d5f40fe81d1a7b8
imnoob012 <ritsuki28@icloud.com>
2025-08-19 06:20:44 +0900

    文字列型から数値型に変換する処理と加算する処理を分離した。

--- 5_Java/src/SeventhProgram.java
+++ 5_Java/src/SeventhProgram.java
@@ -4,15 +4,18 @@ public class SeventhProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int result = calculate(args[0], args[1]);
-		output(args[0], args[1], result);
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+		int result = calculate(num1, num2);
+		
+		output(num1, num2, result);
 	}
 	
-	private static int calculate(String num1, String num2) {
-		 return Integer.parseInt(num1) +Integer.parseInt(num2);
+	private static int calculate(int num1, int num2) {
+		 return num1 + num2;
 	}
 	
-	private static void output(String num1, String num2, int result) {
+	private static void output(int num1, int num2, int result) {
 		System.out.println("一つ目の引数：" + num1);
 		System.out.println("二つ目の引数：" + num2);
 		System.out.println("加算の結果：" + result);
